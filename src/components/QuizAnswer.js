612f37a51efd9179a65fd64c2c17137cc49a68d9
anuj96 <anujjaincoolbuddy@gmail.com>
2020-04-30 02:07:57 +0530

    Answer Page css

--- src/components/QuizAnswer.js
+++ src/components/QuizAnswer.js
@@ -9,93 +9,20 @@ import TableHead from "@material-ui/core/TableHead";
 import TablePagination from "@material-ui/core/TablePagination";
 import TableRow from "@material-ui/core/TableRow";
 
-// This is for the main Table:
-// position: absolute;
-// width: 52%;
-// margin-top: 151px;
-// /* right: 3%; */
-// left: 18%;
 
-// This if for the column row:
-// position: fixed;
-// right: 35px;
-// width: 100%;
-// left: 18%;
-
-const columns = [
-  { id: "name", label: "Name", minWidth: 170 },
-  { id: "code", label: "ISO\u00a0Code", minWidth: 100 },
-  {
-    id: "population",
-    label: "Population",
-    minWidth: 170,
-    align: "right",
-    format: value => value.toLocaleString("en-US")
-  },
-  {
-    id: "size",
-    label: "Size\u00a0(km\u00b2)",
-    minWidth: 170,
-    align: "right",
-    format: value => value.toLocaleString("en-US")
-  },
-  {
-    id: "density",
-    label: "Density",
-    minWidth: 170,
-    align: "right",
-    format: value => value.toFixed(2)
-  }
-];
-
-function createData(name, code, population, size) {
-  const density = population / size;
-  return { name, code, population, size, density };
-}
-
-const rows = [
-  createData("India", "IN", 1324171354, 3287263),
-  createData("China", "CN", 1403500365, 9596961),
-  createData("Italy", "IT", 60483973, 301340),
-  createData("United States", "US", 327167434, 9833520),
-  createData("Canada", "CA", 37602103, 9984670),
-  createData("Australia", "AU", 25475400, 7692024),
-  createData("Germany", "DE", 83019200, 357578),
-  createData("Ireland", "IE", 4857000, 70273),
-  createData("Mexico", "MX", 126577691, 1972550),
-  createData("Japan", "JP", 126317000, 377973),
-  createData("France", "FR", 67022000, 640679),
-  createData("United Kingdom", "GB", 67545757, 242495),
-  createData("Russia", "RU", 146793744, 17098246),
-  createData("Nigeria", "NG", 200962417, 923768),
-  createData("Brazil", "BR", 210147125, 8515767),
-  createData("India", "IN", 1324171354, 3287263),
-  createData("China", "CN", 1403500365, 9596961),
-  createData("Italy", "IT", 60483973, 301340),
-  createData("United States", "US", 327167434, 9833520),
-  createData("Canada", "CA", 37602103, 9984670),
-  createData("Australia", "AU", 25475400, 7692024),
-  createData("Germany", "DE", 83019200, 357578),
-  createData("Ireland", "IE", 4857000, 70273),
-  createData("Mexico", "MX", 126577691, 1972550),
-  createData("Japan", "JP", 126317000, 377973),
-  createData("France", "FR", 67022000, 640679),
-  createData("United Kingdom", "GB", 67545757, 242495),
-  createData("Russia", "RU", 146793744, 17098246),
-  createData("Nigeria", "NG", 200962417, 923768),
-  createData("Brazil", "BR", 210147125, 8515767)
-];
 
 const useStyles = makeStyles({
   tableheading: {
-    width: "100%",
+    width: "%",
     position: "absolute",
-    width: "52%",
     marginTop: 151,
-    left: "18%"
+    left: "19%"
   },
   container: {
-    maxHeight: 440
+    maxHeight: 705
+  },
+  tablecolumns: {
+    background: "blue"
   }
 });
 
@@ -136,19 +63,19 @@ export default function QuizAnswer1(props) {
     <Paper className={classes.tableheading}>
       <TableContainer className={classes.container}>
         <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-            <TableCell key="name" style={{ minWidth: 170 }}>
+          <TableHead className={classes.tablecolumns}>
+            <TableRow >
+            <TableCell key="name" style={{ minWidth: 60 }}>
                 NUMBER
               </TableCell>
-              <TableCell key="name" style={{ minWidth: 170 }}>
+              <TableCell key="name" style={{ minWidth: 200 }}>
                 QUESTIONS
               </TableCell>
-              <TableCell key="code" style={{ minWidth: 100 }}>
+              <TableCell key="code" style={{ minWidth: 60 }}>
                 ANSWER
               </TableCell>
               <TableCell key="population" style={{ minWidth: 170 }}>
-                REMARKS5
+                REMARKS
               </TableCell>
             </TableRow>
           </TableHead>
